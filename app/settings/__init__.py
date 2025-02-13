303765f3f461a27eeecf2cf9a91b8dd83ee2d521
erfjab <erfjab@gmail.com>
2025-02-14 03:29:53 +0330

    feat: env file reader

---
+++ app/settings/__init__.py
@@ -0,0 +1,4 @@
+from ._log import logger
+from ._config import env
+
+__all__ = ["logger", "env"]
---
+++ app/settings/_config/_env.py
@@ -0,0 +1,9 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class EnvSettingsFile(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env", case_sensitive=True, extra="ignore"
+    )
+    BOT_TOKEN: str = ""
+    GIT_TOKEN: list[int] = []
